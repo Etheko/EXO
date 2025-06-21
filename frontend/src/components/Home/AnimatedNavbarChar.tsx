cdcfeb3fcd270809db1e00be5200f4d273e6936d
Etheko. <i.ruizba.2021@alumnos.urjc.es>
2025-06-21 16:59:49 +0200

    Navbar update

--- frontend/src/components/Home/AnimatedNavbarChar.tsx
+++ frontend/src/components/Home/AnimatedNavbarChar.tsx
@@ -10,23 +10,33 @@ const AnimatedNavbarChar = ({ char }: AnimatedNavbarCharProps) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const { transform, fontFamily } = useSpring({
-    transform: `scale(${isHovered ? 1.4 : 1})`,
+    transform: `scale(${isHovered ? 1.2 : 1})`,
     fontFamily: isHovered ? 'Jua, sans-serif' : 'Space Grotesk, sans-serif',
     config: { mass: 0.5, tension: 400, friction: 15 },
   });
 
+  const containerStyle = {
+    marginLeft: char === 'e' ? '1.2px' : '0',
+    marginRight: char === 'e' ? '0.5px' : '0',
+  };
+
   return (
-    <animated.span
-      style={{
-        display: 'inline-block',
-        transform,
-        fontFamily,
-      }}
+    <div
+      className="char-container"
+      style={containerStyle}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      {char}
-    </animated.span>
+      <animated.span
+        style={{
+          display: 'inline-block',
+          transform,
+          fontFamily,
+        }}
+      >
+        {char}
+      </animated.span>
+    </div>
   );
 };
 
